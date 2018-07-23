c7624e8daa1f897bfc273b53fb7cf948aa685839
Hagai Shabi <cl_hagaish@rnd-hub.com>
2018-07-23 12:11:57 +0300

    Adding combo to switch between the types

--- src/App.js
+++ src/App.js
@@ -2,19 +2,76 @@ import React, { Component } from 'react';
 import FileViewer from 'react-file-viewer';
 import './App.css';
 
+import sampleHouse from './example_files/SampleHouse.wexbim';
+import solarImage from './example_files/02-USVI-Solar.jpg';
+import docx from './example_files/SampleSpec.docx';
+import doc from './example_files/sample.doc';
+import csv from './example_files/Total_Crime.csv';
+import mp4 from './example_files/small.mp4';
+import xlsx from './example_files/SimpleSpreadsheet.xlsx';
+import photo360 from './example_files/360photo.jpg';
+import avi from './example_files/drop.avi';
+import webm from './example_files/small.webm';
+import mov from './example_files/step.mov';
+import mp3 from './example_files/sample.mp3';
+import rtf from './example_files/sample.rtf';
+import pdf from './example_files/sample.pdf';
+
 class App extends Component {
-  state = {};
+  types = [
+    { name: "pdf", value: pdf },
+    { name: "docx", value: docx },
+    { name: "mp4", value: mp4 },
+    { name: "mp3", value: mp3 },
+    { name: "webm", value: webm },
+    { name: "wexbim", value: sampleHouse }
+  ];
+
+  constructor() {
+    super();
+    this.state = {
+      path: this.types[0].value,
+      type: this.types[0].name
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  createSelectItems() {
+    let items = [];
+
+    for (let i = 0; i < this.types.length; i++) {
+      items.push(<option name={this.types[i].name} value={this.types[i].value}>{this.types[i].name}</option>);
+    }
+
+    return items;
+  }
 
   onError = (err) => {
     console.log(err);
   };
 
+
+  handleChange(e) {
+    let current = this.types[e.target.selectedIndex];
+    this.setState({ path: current.value, type: current.name });
+  }
+
   render() {
     return (
       <div className="container">
         <div className="links">
-          Add links for the app capabilities (pdf, excel, video and others)
-          {/* each link would change the app state and display something else inside the content */}
+          {/* each link would change the app state and display something else inside the content */
+            <div>
+              <select value={this.state.gender} onChange={this.handleChange}>
+                {/* <option name="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"> mp4</option>
+                <option name="http://www.africau.edu/images/default/sample.pdf">pdf</option> */
+
+
+                  this.createSelectItems()}
+
+              </select>
+            </div>
+          }
         </div>
         <div className="content">
           {/* should read state. my suggestion is to add a component that will wrap the react-file-reader and would display different files based on props */}
@@ -26,8 +83,6 @@ class App extends Component {
             */}
             <video
               data-dashjs-player
-
-              autoPlay
               src="https://vm2.dashif.org/livesim-chunked/chunkdur_1/ato_7/testpic4_8s/Manifest300.mpd"
               controls>
               Not supported!
@@ -36,9 +91,9 @@ class App extends Component {
           <div>
             {/* example static video (url or local file) use whatever comes on your mind */}
             <FileViewer
-              fileType={'mp4'}
-              filePath={'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'}
-              onError={this.onError}/>
+              fileType={this.state.type}
+              filePath={this.state.path}
+              onError={this.onError} />
           </div>
         </div>
       </div>
